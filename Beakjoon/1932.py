794b3d2f1eb98a52c0c2bb87e429d3e8eda91fe4
kang <rilac1@naver.com>
2021-06-18 16:00:27 +0900

    210618

--- Beakjoon/1932.py
+++ Beakjoon/1932.py
@@ -1,14 +1,10 @@
 # 정수 삼각형
 n = int(input())
 data = []
-data.append(int(input()))
-for _ in range(1,n):
+for _ in range(n):
     data.append(list(map(int, input().split())))
-
-data[1][0] += data[0]
-data[1][1] += data[0]
-
-for i in range(2,n):
+    
+for i in range(1,n):
     data[i][0] += data[i-1][0]
     for j in range(1,i):
         data[i][j] += max(data[i-1][j], data[i-1][j-1])
