577b705b0dc913234911fe613f89dd80f357b209
kang <rilac1@naver.com>
2021-06-29 12:37:44 +0900

    210629

---
+++ Beakjoon/2581.py
@@ -0,0 +1,26 @@
+# 소수
+# 에라토스테네스의 체
+m = int(input())
+n = int(input())
+a = range(10001)
+if m==1:
+    m+=1
+
+for i in a[2:]:
+    if i == 0:
+        continue
+    temp = i*2
+    while temp < 10001:
+        a[temp] = 0
+        temp += i
+
+prime = []
+for p in a[m:n+1]:
+    if not p==0:
+        prime.append(p)
+
+if len(prime) == 0:
+    print(-1)
+else:
+    print(sum(prime))
+    print(prime[0])
