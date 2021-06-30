a47c595f872cdcaf4ecc5807558f260728050162
kang <rilac1@naver.com>
2021-06-30 20:43:24 +0900

    210630

--- Beakjoon/14888_미완.py
+++ Beakjoon/14888_미완.py
@@ -6,40 +6,21 @@ import itertools
 n = int(input())
 a = list(map(int, input().split()))
 o = list(map(int, input().split()))
-operation = []
-for i, x in enumerate(o):
-    while x>0:
-        operation.append(i)
-        x -= 1
 
 maxV = 0
 minV = 1e9
 
-def calculate(a,op,b):
-    if op==0:
-        return a+b
-    elif op==1:
-        return a-b
-    elif op==2:
-        return a*b
-    elif op==3:
-        if (a >= 0 and b>0) or (a<=0 and b<0) :
-            return a//b
-        elif a<0:
-            return -(-a//b)
-        elif b<0:
-            return -(a//-b)
+def back(x, y, index):
+    if index == n-1:
+        maxV = max(maxV, x)
+        minV = min(minV, x) 
 
+    num = n[index]
+    x += num
+    back(x, index+1)
 
-temp = a.pop(0)
-for perm in itertools.permutations(operation):
-    x = temp
-    for i in range(n-1):
-        x = calculate(x, perm[i], a[i])
-    if x>maxV:
-        maxV = x
-    if x<minV:
-        minV = x
+back(a[0], 1)
+
 
 print(maxV)
 print(minV)
--- Beakjoon/backtracking/15640.py
+++
@@ -1,4 +0,0 @@
-# N과 M (1)
-n, m = map(int(input()))
-arr = range(1,n+1)
-
