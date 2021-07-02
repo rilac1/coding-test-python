e953caf74b49dba552c54dd250d5d812cf46a462
kang <rilac1@naver.com>
2021-07-02 10:09:08 +0900

    210702

--- Beakjoon/14888_미완.py
+++ Beakjoon/14888_미완.py
@@ -10,21 +10,27 @@ maxV = 0
 minV = 1e9
 
 def cal(x, i):
-    if i==n-1:
+    if i == n-1:
         maxV = max(maxV, x)
         minV = min(minV, x)
-
+        return x
     if o[0]>0:
         o[0] -= 1
-        return cal(x, x + a[i], i+1)
+        x = cal(x + a[i], i+1) - a[i]
+        o[0] += 1
     if o[1]>0:
         o[1] -= 1
-        return cal(x, x - a[i], i+1)
+        x = cal(x - a[i], i+1) + a[i]
+        o[1] = 1
     if o[2]>0:
         o[2] -= 1
-        return cal(x, x * a[i], i+1)
+        x = cal(x * a[i], i+1) // a[i]
+        o[2] += 1
     if o[3]>0:
         o[3] -= 1
-    
+        x = cal (x // a[i], i+1) * a[i]
+        o[3] += 1
+
+cal(a[0], 1)
 print(maxV)
 print(minV)
