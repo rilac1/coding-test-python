9e2377b73d186263edb98431495fa4604b3c6a7c
kang <rilac1@naver.com>
2021-07-06 15:46:55 +0900

    210706

--- Beakjoon/1062.py
+++ Beakjoon/1062.py
@@ -3,13 +3,16 @@ import sys
 input = sys.stdin.readline
 N, K = map(int, input().split())
 words = [[] for _ in range(N)]
+alphabet = set()
 for i in range(N):
     temp = (list(input().strip()))
-    words[i] = temp[4:-4]
+    temp = temp[4:-4]
+    words[i] = temp
+    alphabet.add(temp)
 
 if K<5:
     print(0)
     exit(0)
 learn = ['a','c','n','t','i']
 
-print(words)
+print(alphabet)
