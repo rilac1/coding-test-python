2836c91e99e2ace7b68729c00b7544fe2ae11c23
kang <rilac1@naver.com>
2021-07-07 20:18:58 +0900

    210707

--- Beakjoon/DP/15486.py
+++ Beakjoon/DP/15486.py
@@ -1,1 +1,14 @@
-# 퇴사
+# 퇴사2
+import sys
+input = sys.stdin.readline
+N = int(input())
+table = [[] for _ in range(N+1)]
+
+for i in range(1, N+1):
+    t,p = map(int, input().split())
+    table[i] = (t, p)
+dp = [[0] * (N+1) for _ in range(N+1)]
+
+for i in range(1, N+1):         # 손님종류 = 현재시간
+    for j in range(1, N+1):
+        ...
