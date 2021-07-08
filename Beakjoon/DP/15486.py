fd815a22691ecadfa66cc7fe8f658555b6273b71
kang <rilac1@naver.com>
2021-07-08 14:10:22 +0900

    210708

--- Beakjoon/DP/15486.py
+++ Beakjoon/DP/15486.py
@@ -7,8 +7,13 @@ table = [[] for _ in range(N+1)]
 for i in range(1, N+1):
     t,p = map(int, input().split())
     table[i] = (t, p)
-dp = [[0] * (N+1) for _ in range(N+1)]
+dp = [0] * (N+1)
 
-for i in range(1, N+1):         # 손님종류 = 현재시간
-    for j in range(1, N+1):
-        ...
+for i in range(1, N+1):
+    dp[i] = max(dp[i-1], dp[i])
+    time = table[i][0] - 1
+    price = table[i][1]
+    if i + time <= N:
+        dp[i+time] = max(dp[i+time], dp[i-1]+price)
+
+print(dp[N])
