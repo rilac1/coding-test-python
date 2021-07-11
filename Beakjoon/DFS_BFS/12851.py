88fe730a59196d9c851615a3fc194fa88775bfdb
kang <rilac1@naver.com>
2021-07-11 23:29:40 +0900

    210711

--- Beakjoon/DFS_BFS/12851.py
+++ Beakjoon/DFS_BFS/12851.py
@@ -11,13 +11,13 @@ queue = deque([N])
 while queue:
     x = queue.popleft()
     for i in (x+1, x-1, x*2):
-        if 0<=i<=100000:
-            if min_path[i] == -1:
-                min_path[i] = min_path[x] + 1
+        if 0<=i<100001:
+            if min_path[i]==-1:
+                min_path[i] = min_path[x]+1
                 path_num[i] = path_num[x]
                 queue.append(i)
-            elif min_path[i] == min_path[x]+1:
-                path_num[i] += path_num[x]
+            elif min_path[N] == min_path[x]+1:
+                path_num[N] += path_num[x]
 
 print(min_path[K])
 print(path_num[K])
