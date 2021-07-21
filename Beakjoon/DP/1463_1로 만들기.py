942bf18de262b98297596b44fae665ed70a03061
kang <rilac1@naver.com>
2021-07-21 20:23:03 +0900

    210721

--- Beakjoon/DP/1463_1로 만들기.py
+++ Beakjoon/DP/1463_1로 만들기.py
@@ -2,14 +2,10 @@ N = int(input())
 dp = [0] * (N+1)
 
 for i in range(2, N+1):
-    temp=True
+    dp[i] = dp[i-1]+1
     if i%2==0:
-        dp[i] = min(dp[i//2]+1, dp[i-1]+1)
-        temp=False
+        dp[i] = min(dp[i//2]+1, dp[i])
     if i%3==0:
-        dp[i] = min(dp[i//3]+1, dp[i-1]+1)
-        temp=False
-    if temp:
-        dp[i] = dp[i-1]+1
+        dp[i] = min(dp[i//3]+1, dp[i])
 
 print(dp[N])
