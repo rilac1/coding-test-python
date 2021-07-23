86a87960bcd5e8b84a7be5fd8d386d9a087445f4
kang <rilac1@naver.com>
2021-07-24 08:47:45 +0900

    210724

---
+++ Beakjoon/DFS-BFS/1260_DFS와 BFS.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, M, V = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+for i in graph:
+    i.sort()
+
+visited = [False]*(N+1)
+def dfs(a):
+    if visited[a]:
+        return
+    print(a, end=' ')
+    visited[a] = True
+    for i in graph[a]:
+        dfs(i)
+dfs(V)
+print()
+
+visited = [False]*(N+1)
+queue = deque([V])
+while queue:
+    a = queue.popleft()
+    if visited[a]:
+        continue
+    print(a, end=' ')
+    visited[a] = True
+    for i in graph[a]:
+        queue.append(i)
+print()
