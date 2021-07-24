3c11c26d1544c511116d7c7f8c68170d0f5e68fb
kang <rilac1@naver.com>
2021-07-24 09:27:11 +0900

    210724

--- Beakjoon/DP/1495_temp.py
+++ Beakjoon/DP/1495_temp.py
@@ -3,20 +3,3 @@ input = sys.stdin.readline
 N, S, M = map(int, input().split())
 V = list(map(int, input().split()))
 
-def dfs(s, n):
-    global ret
-    global last
-    if not 0<=s<=M:
-        return
-    if n==N:
-        ret = max(ret, s)
-        last = True
-        return
-    dfs(s+V[n], n+1)
-    dfs(s-V[n], n+1)
-
-ret = 0
-last = False
-dfs(S, 0)
-if last:
-    print(ret)
