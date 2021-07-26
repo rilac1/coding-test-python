bf47a7b8eb61bf1058193f559c42248f42f50f42
kang <rilac1@naver.com>
2021-07-26 21:57:04 +0900

    210726

--- Beakjoon/Gridy/1789.py
+++ Beakjoon/Gridy/1789.py
@@ -1,10 +1,12 @@
 # 수들의 합
 S = int(input())
 
-cnt = 1
+cnt = 0
 start = 0
-while start<S:
+while start<=S:
+    cnt += 1
     start += cnt
+if start==S:
     cnt += 1
 
-print(cnt)
+print(cnt, start)
