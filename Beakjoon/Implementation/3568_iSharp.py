bd9ad11684543f1c940750baf657b103f3d7bbd8
kang <rilac1@naver.com>
2021-07-31 21:15:49 +0900

    210731

--- Beakjoon/Implementation/3568_iSharp.py
+++ Beakjoon/Implementation/3568_iSharp.py
@@ -1,15 +1,19 @@
-# isharP
+# iSharp
 import copy
 line = input().split()
 
 for parts in line[1:]:
     type = copy.deepcopy(line[0])
     name = ''
-    for i in parts:
+    for i in parts[::-1]:
         if i == ',' or i==';':
             continue
         elif 'A'<= i <='Z' or 'a'<= i <='z':
             name = name+i
+        elif i == '[':
+            type = type + ']'
+        elif i == ']':
+            type = type + '['
         else:
             type = type + i
-    print(type, name+';')
+    print(type, name[::-1]+';')
