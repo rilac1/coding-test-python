f1d02bd77172d663a0e91086120c5f860de93d9a
kang <rilac1@naver.com>
2021-08-10 12:20:54 +0900

    210810

--- Beakjoon/Implementation/14503_로봇 청소기.py
+++ Beakjoon/Implementation/14503_로봇 청소기.py
@@ -1,3 +1,4 @@
+# 로봇 청소기
 import sys
 input = sys.stdin.readline
 
@@ -7,22 +8,26 @@ stage = []
 for _ in range(N):
     stage.append(list(map(int, input().split())))
 
-dx = [0, 1, 0, -1]
-dy = [-1, 0, 1, 0]
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 ret = 0
 
 while True:
-    if stage[r][c]==1:
-        break
-    if stage[r][c]==0:
+    if stage[r][c] == 0:
         stage[r][c] = 2
         ret += 1
+    if stage[r][c] == 1:
+        break
+
     for i in range(4):    
         d = (d-1)%4
-        if stage[r+dx[d]][c+dy[d]]==0:
+        x = r+dx[d]
+        y = c+dy[d]
+        
+        if stage[x][y]==0:
+            r, c = x, y
             break
         if i==3:
-            d = (d-1)%4
             r -= dx[d]
             c -= dy[d]
 
