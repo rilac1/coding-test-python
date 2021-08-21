747a0ee945dc2908f765bc448b3fd1453b15dc85
kang <rilac1@naver.com>
2021-08-21 21:40:28 +0900

    210821

--- Beakjoon/DFS-BFS/10026_적록색약.py
+++ Beakjoon/DFS-BFS/10026_적록색약.py
@@ -14,7 +14,7 @@ for i in range(N):
 
 dx = [0,1,0,-1]
 dy = [-1,0,1,0]
-a,b = 0,0
+ret = [0,0]
 
 def dfs(x,y,color):
     graph[x][y] = 0
@@ -31,6 +31,7 @@ for i in range(N):
         if color:
             a += 1
             dfs(i,j,color)
+
         graph = graph2
         color = graph[i][j]
         if color:
