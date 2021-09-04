4205e323b9fb3e8b96cb23b0cf9e297a7bdc0998
kang <rilac1@naver.com>
2021-09-04 15:07:08 +0900

    210904

---
+++ Beakjoon/Graph/1753_최단경로.py
@@ -0,0 +1,26 @@
+# 최단경로
+import sys, heapq
+input = sys.stdin.readline
+
+V, E = map(int, input().split())
+K = int(input())
+graph = [[]for _ in range(V+1)]
+for i in range(E):
+    u,v,w = map(int, input().split())
+    graph[u].append((v,w))
+
+dist = [1e9] * (V+1)
+h = []
+heapq.heappush(h, (0, K))
+while h:
+    cost, next = heapq.heappop(h)
+    if cost < dist[next]:
+        dist[next] = cost
+        for n, c in graph[next]:
+            heapq.heappush(h, (c+cost, n))
+
+for i in dist[1:]:
+    if i == 1e9:
+        print('INF')
+    else:
+        print(i)
