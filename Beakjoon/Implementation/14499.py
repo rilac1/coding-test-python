a026ba1ae857e8a0dc82853b7fed6a05d3912f97
kang <rilac1@naver.com>
2021-09-06 23:24:40 +0900

    210906

--- Beakjoon/Implementation/14499.py
+++ Beakjoon/Implementation/14499.py
@@ -11,6 +11,17 @@ dice = range(1,7)
 def move(x,y,n,d):
     nx, ny = x+dx[n], y+dy[n]
     if not 0<=nx<N or not 0<=ny<M:
-        return
+        return d
     if graph[nx][ny] == 0:
         graph[nx][ny]
+
+# 0 2 0
+# 4 1 3
+# 0 5 0
+# 0 6 0
+
+# 동쪽으로
+# 1 3 6 4
+# 북쪽으로
+# 1 2 6 5
+
