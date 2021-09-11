398e30c4c9a686fbfeef69e0121479bed341f634
kang <rilac1@naver.com>
2021-09-11 15:29:32 +0900

    210911

--- Beakjoon/Gridy/1202.py
+++ Beakjoon/Gridy/1202.py
@@ -1,38 +1,18 @@
-import sys
+# 보석 도둑
+import sys, heapq
 input = sys.stdin.readline
 N, K = map(int, input().split())
-jewel = []
-for _ in range(N):
-    M, V = map(int, input().split())
-    jewel.append((V, M))
+jewel = [list(map(int, input().split())) for _ in range(N)]
 bag = [int(input()) for _ in range(K)]
 
 jewel.sort()
 bag.sort()
 ret = 0
-while jewel and bag:
-    v, m = jewel.pop()
-    start, end = 0, len(bag)-1
-    while start < end:
-        mid = (start+end)//2
-        if m == bag[mid]:
-            start = end = mid
-        elif m < bag[mid]:
-            end = mid
-        else:
-            start = mid + 1
-    if m <= bag[end]:
-        ret += v
-        bag.pop(end)
-
+h = []
+for b in bag:
+    while jewel and b >= jewel[0][0]:
+        heapq.heappush(h, -jewel[0][1])
+        heapq.heappop(jewel)
+    if h:
+        ret -= heapq.heappop(h)
 print(ret)
-
-# 4 4
-# 1 100
-# 2 200
-# 13 300
-# 10 500
-# 10
-# 10
-# 10
-# 14
