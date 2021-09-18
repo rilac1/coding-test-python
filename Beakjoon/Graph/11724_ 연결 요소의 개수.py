de4cbf0da6e6a70479860002ed86eb7d975051d8
JEONG HYUN KANG <rilac1@naver.com>
2021-09-18 15:04:43 +0900

    210918

---
+++ Beakjoon/Graph/11724_ 연결 요소의 개수.py
@@ -0,0 +1,25 @@
+# 연결 요소의 개수
+import sys
+sys.setrecursionlimit(100000)
+input = sys.stdin.readline
+N, M = map(int, input().split())
+graph = [[]for _ in range(N+1)]
+for _ in range(M):
+	u,v = map(int, input().split())
+	graph[u].append(v)
+	graph[v].append(u)	
+
+def dfs(a):
+		visited[a]=True
+		for i in graph[a]:
+			if not visited[i]:
+				dfs(i)
+
+ret = 0
+visited = [False]*(N+1)
+for i in range(1,N+1):
+	if not visited[i]:
+		ret += 1
+		dfs(i)
+		
+print(ret)
