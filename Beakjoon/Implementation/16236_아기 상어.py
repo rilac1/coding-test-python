9ff1ed0bf2a5ae17aa9a72fc359021c685350932
kang <rilac1@naver.com>
2021-09-19 12:36:46 +0900

    210919

--- Beakjoon/Implementation/16236_아기 상어.py
+++ Beakjoon/Implementation/16236_아기 상어.py
@@ -1,8 +1,7 @@
 # 아기 상어
-import sys
+import sys, heapq
 from collections import deque
 input = sys.stdin.readline
-
 N = int(input())
 graph = [list(map(int, input().split())) for _ in range(N)]
 q = deque()
@@ -16,35 +15,34 @@ dx = [-1,0,0,1]
 dy = [0,-1,1,0]
 size,eat,ret,limit = 2,0,0,1e9
 visited = [[False]*N for _ in range(N)]
-fish = []
 
+fish=[]
 while q or fish:
     if not q and fish:
-        fish.sort()
-        dis,a,b=fish[0]
-        graph[a][b]=0
-        eat += 1
-        if eat==size:
-            eat = 0
-            size += 1
-        
-        visited = [[False]*N for _ in range(N)]
-        limit = 1e9
-        ret += dis+1
-        deque.append(q, (a,b,0))
-        continue
+        nx,ny = fish[0]
+        fish=[]
 
-    x, y, cnt = deque.popleft(q)
-    visited[x][y] = True
-    if cnt>=limit: 
+        graph[nx][ny]=0
+        eat+=1
+        if eat==size:
+            eat=0
+            size+=1
+    
+        visited = [[False]*N for _ in range(N)]
+        ret += limit
+        limit=1e9
+        deque.append(q, (nx,ny,0))
         continue
+    
+    x,y,cnt = deque.popleft(q)
     for i in range(4):
         nx,ny = x+dx[i], y+dy[i]
-        if 0<=nx<N and 0<=ny<N and graph[nx][ny]<=size:
-            if 0<graph[nx][ny]<size:
-                if not fish: limit = cnt
-                fish.append((cnt,nx,ny))
-
-            if not visited[nx][ny]:
+        if cnt<limit and 0<=nx<N and 0<=ny<N and not visited[nx][ny]:
+            visited[nx][ny] = True
+            temp=graph[nx][ny]
+            if 0<temp<size:
+                limit = cnt+1
+                heapq.heappush(fish, (nx,ny))
+            elif temp==0 or temp==size:
                 deque.append(q, (nx,ny,cnt+1))
 print(ret)
