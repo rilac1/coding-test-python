0d4f5926e5b143d73f84963474e4fc7b1219ce5a
kang <rilac1@naver.com>
2021-10-03 02:05:44 +0900

    211003

---
+++ Beakjoon/1912.py
@@ -0,0 +1,4 @@
+# 연속합
+N = int(input())
+arr = list(map(int,input().split()))
+dp = []
---
+++ Beakjoon/etc/20040_사이클 게임.py
@@ -0,0 +1,25 @@
+# 사이클 게임
+import sys
+input = sys.stdin.readline
+n, m = map(int,input().split())
+action = [tuple(map(int, input().split())) for _ in range(m)]
+
+def find(x):
+    if x == parent[x]:
+        return x
+    parent[x] = find(parent[x])
+    return parent[x]
+def union(x,y):
+    x, y = find(x), find(y)
+    if x != y: 
+        parent[y] = x
+        return False
+    else:
+        return True
+
+parent = list(range(0,n))
+for i,(a,b) in enumerate(action):
+    if union(a,b):
+        print(i+1)
+        break
+else: print(0)
