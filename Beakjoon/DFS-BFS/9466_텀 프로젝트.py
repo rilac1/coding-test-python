35cfc0b9fa032ccc97fd8a5a87857c1f375cbfc8
kang <rilac1@naver.com>
2021-10-07 21:27:12 +0900

    211007

---
+++ Beakjoon/DFS-BFS/9466_텀 프로젝트.py
@@ -0,0 +1,22 @@
+# 텀 프로젝트
+def dfs(x):
+    visited[x] = True
+    ring.append(x)
+    if visited[next[x]]:
+        if next[x] in ring: 
+            return len(ring) - ring.index(next[x])
+        else: 
+            return 0
+    return dfs(next[x])
+    
+T = int(input())
+for _ in range(T):
+    ret = 0
+    n = int(input())
+    next = [0] + list(map(int, input().split()))
+    visited = [False]*(n+1)
+    for s in range(1,n+1):
+        if not visited[s]:
+            ring = [] 
+            ret += dfs(s)
+    print(n-ret)
