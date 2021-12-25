0419d0691288f9eaeba18f2eb0aae762e285f9b6
kang <rilac1@naver.com>
2021-12-25 23:33:49 +0900

    binary search

---
+++ 1920_수 찾기.py
@@ -0,0 +1,16 @@
+# 수 찾기
+N = int(input())
+a = sorted(map(int, input().split()))
+M = int(input())
+r = list(map(int, input().split()))
+
+def bisect(l,r):
+    global t
+    while l<r:
+        m = (l+r)//2
+        if t<a[m]: r = m
+        elif t>a[m]: l = m+1
+        else: return 1
+    return 0
+
+for t in r: print(bisect(0, N))
