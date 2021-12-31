3563894d9298e2e2b1ceac2055d3435a76426a60
kang <rilac1@naver.com>
2021-12-31 14:50:36 +0900

    backup

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
-def bisect(left, right):
-    global target
-    while left <= right:
-        mid = (left+right)//2
-        if arr[mid] < target:
-            left = mid+1
-        elif arr[mid] > target:
-            right = mid-1
-        else: return 1
-    return 0
+N = int(input())
+A = list(map(int,input().split()))
+for i in range(1,N): A[i] += A[i-1]
 
-arr = [1,2,3,4,5]
-target = 2
-print(bisect(0,4))
+M = int(input())
+for _ in range(M):
+    i,j = map(int, input().split())
+    d = {0:-1}
+    ans = 0
+    for inx in range(i-1,j):
+        if A[inx] in d: ans = max(ans, inx-d[A[inx]])
+        else: d[A[inx]] = inx
+    print(ans)
