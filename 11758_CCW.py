ab6346037a6a8a1306a64de703ccbf51e8291fa0
kang <rilac1@naver.com>
2022-01-15 23:57:05 +0900

    220114

--- 11758_CCW.py
+++ 11758_CCW.py
@@ -4,5 +4,5 @@ x, y = [i[0]for i in P], [i[1]for i in P]
 r = (x[0]*y[1] + x[1]*y[2] + x[2]*y[0])\
     - (x[1]*y[0] + x[2]*y[1] + x[0]*y[2])
 if r>0: print(1)
-elif r<0: prin      t(-1)
+elif r<0: print(-1)
 else: print(0)
---
+++ 9012.py
@@ -0,0 +1,8 @@
+from inspect import stack
+import sys
+input = sys.stdin.readline
+T = int(input())
+
+stk = []
+for i in range(T):
+    vps = input().rstrip()
