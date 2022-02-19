a824cae14422e4388eda12508168b72233eb5cd3
kang <rilac1@naver.com>
2022-02-20 00:09:51 +0900

    220219

--- 14500.py
+++ 14500.py
@@ -2,30 +2,30 @@ import sys
 input = sys.stdin.readline
 N,M = map(int,input().split())
 graph = [list(map(int,input().split())) for _ in range(N)]
-visited = [[False]*M for _ in range(N)]
 
-dx,dy = [1,0], [0,1]
+visited = [[False]*M for _ in range(N)]
+dx,dy = [1,0,-1,0], [0,1,0,-1]
 ans = 0
+
 def dfs(x, y, val, cnt):
     global ans
-
     visited[x][y] = True
+
     val += graph[x][y]
     cnt += 1
 
     if cnt==4: 
         print_check()
         ans = max(ans, val)
-    elif cnt==2:
-        
+        return
 
-    else:
-        for i in range(2):
-            nx, ny = x+dx[i], y+dy[i]
-            if 0<=nx<N and 0<=ny<M:
-                if not visited[nx][ny]:
-                    dfs(nx,ny,val,cnt)
-        
+    for i in range(2):
+        nx, ny = x+dx[i], y+dy[i]
+        if 0<=nx<N and 0<=ny<M:
+            if not visited[nx][ny]:
+                dfs(nx,ny,val,cnt)
+    if cnt==2: hiddenBlock(x,y,val,0)
+
     visited[x][y] = False
     return
 
@@ -34,6 +34,19 @@ for i in range(N):
         dfs(i,j,0,0)
 print(ans)
 
+def hiddenBlock(x,y,val,cnt):
+    if cnt==2:
+        print_check()
+        ans = max(ans, val)
+
+    for i in range(4):
+        nx, ny = x+dx[i], y+dy[i]
+        if 0<=nx<N and 0<=ny<M:
+            if not visited[nx][ny]:
+                visited[nx][ny] = True
+                hiddenBlock(x,y,val+graph[nx][ny],1)
+                visited[nx][ny] = False
+                
 
 def print_check():
     for v1 in visited:
