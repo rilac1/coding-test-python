a2fe377a7de677f5d9958b68b91798ce1080422a
kang <rilac1@naver.com>
2022-02-19 23:53:41 +0900

    220219

--- 14500.py
+++ 14500.py
@@ -1,22 +1,43 @@
-from collections import deque
+import sys
+input = sys.stdin.readline
 N,M = map(int,input().split())
 graph = [list(map(int,input().split())) for _ in range(N)]
+visited = [[False]*M for _ in range(N)]
 
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
-
+dx,dy = [1,0], [0,1]
 ans = 0
-q = deque([(0,0,0,0)])
-while q:
-    x,y,cnt,value = q.popleft()
-    if cnt==4:
-        ans = max(ans,value)
-        continue
-    
-    for i in range(4):
-        nx = x+dx[i]
-        ny = y+dy[i]
-        if 0<=nx<N and 0<=ny<M:
-            q.append((nx,ny,cnt+1, value+graph[x][y]))
+def dfs(x, y, val, cnt):
+    global ans
+
+    visited[x][y] = True
+    val += graph[x][y]
+    cnt += 1
+
+    if cnt==4: 
+        print_check()
+        ans = max(ans, val)
+    elif cnt==2:
         
+
+    else:
+        for i in range(2):
+            nx, ny = x+dx[i], y+dy[i]
+            if 0<=nx<N and 0<=ny<M:
+                if not visited[nx][ny]:
+                    dfs(nx,ny,val,cnt)
+        
+    visited[x][y] = False
+    return
+
+for i in range(N):
+    for j in range(M):
+        dfs(i,j,0,0)
 print(ans)
+
+
+def print_check():
+    for v1 in visited:
+        for v2 in v1: 
+            print(int(v2), end =' ')
+        print()
+    print()
