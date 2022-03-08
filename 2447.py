aa4166fd6a41e0e3d5a7d54625a741fb8e755c59
kang <rilac1@naver.com>
2022-03-09 00:02:20 +0900

    220308

--- 2447.py
+++ 2447.py
@@ -2,21 +2,14 @@ N = int(input())
 
 def dfs(n,col,row):
     if n==1:
-        print('*',end='')
-        if row==N-1: print()
+        if col==1 and row==1: print(' ', end='')
+        else: print('*',end='')
         return
 
     n //= 3
-    dfs(n,col,row)
-    dfs(n,col,row+n)
-    dfs(n,col,row+n+n)
-    
-    dfs(n,col+n,row)
-    dfs(n,col+n,row+n+n)
-
-    dfs(n,col+n+n,row)
-    dfs(n,col+n+n,row+n)
-    dfs(n,col+n+n,row+n+n)
-
+    for i in range(3):
+        for j in range(3):
+            dfs(n,col+i,row+j)
+    print()
 
 dfs(N,0,0)
